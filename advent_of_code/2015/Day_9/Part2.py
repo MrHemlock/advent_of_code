63ee755b182e116d878f90398b297a6383726995
Daniel Brown <browndj3@gmail.com>
2019-02-22 15:37:59 -0600

    Day 9 completed.

---
+++ advent_of_code/2015/Day_9/Part2.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+
+cities = {}
+best = None
+
+with open('input9.txt') as file:
+    raw_data = [line.split() for line in file]
+
+for group in raw_data:
+    if group[0] not in cities:
+        cities[group[0]] = {}
+    if group[2] not in cities:
+        cities[group[2]] = {}
+    cities[group[0]][group[2]] = int(group[-1])
+    cities[group[2]][group[0]] = int(group[-1])
+
+groupings = [group for group in permutations(cities.keys())]
+
+for group in groupings:
+    total = 0
+    for index in range(len(group)-1):
+        total += cities[group[index]][group[index+1]]
+    if best is None or best < total:
+        best = total
+print(best)
