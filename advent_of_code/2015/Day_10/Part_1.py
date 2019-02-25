68f5309cf25677b960a1c8be487309d47388c88f
Daniel Brown <browndj3@gmail.com>
2019-02-25 16:26:11 -0600

    Day 10 and day 11 completed.

---
+++ advent_of_code/2015/Day_10/Part_1.py
@@ -0,0 +1,33 @@
+previous_num = None
+
+
+with open("input10.txt") as file:
+    data = file.read().strip()
+
+for _ in range(40):
+    new_string = ""
+    for index, num in enumerate(data):
+        total = 1
+        temp_index = index
+        flag = False
+        while True:
+            try:
+                if previous_num is None or previous_num != num:
+                    if num == data[temp_index+1]:
+                        temp_index += 1
+                        total += 1
+                    else:
+                        break
+                else:
+                    flag = True
+                    break
+            except IndexError:
+                break
+        previous_num = num
+        if not flag:
+            new_string += str(total) + num
+    data = new_string
+    previous_num = None
+
+
+print(len(data))
---
+++ advent_of_code/2015/Day_11/Part2.py
@@ -0,0 +1,45 @@
+from string import ascii_lowercase
+
+data = "hepxxzaa"
+bad_letters = "iol"
+straights = set()
+doubles = set((let * 2 for let in ascii_lowercase if let not in bad_letters))
+cleaned_letters = tuple(let for let in ascii_lowercase if let not in bad_letters)
+next_letter = dict(zip("abcdefghjkmnpqrstuvwxyz", "bcdefghjkmnpqrstuvwxyza"))
+
+
+def increment_letters(code):
+    if code[-1] == 'z':
+        return increment_letters(code[:-1]) + 'a'
+    else:
+        return code[:-1] + next_letter[code[-1]]
+
+
+for index in range(len(ascii_lowercase)-2):
+    if not any((let in ascii_lowercase[index:index+3] for let in bad_letters)):
+        straights.add(ascii_lowercase[index:index+3])
+
+while True:
+
+    print(data)
+
+    for i in range(len(data)-2):
+        if data[i:i+3] in straights:
+            break
+    else:
+        data = increment_letters(data)
+        continue
+
+    count = 0
+    for i in range(len(data)-1):
+        if data[i:i+2] in doubles and data[i:i+2] != data[i+1:i+3]:
+            count += 1
+        if count == 2:
+            break
+    else:
+        data = increment_letters(data)
+        continue
+
+    break
+
+print(data)
