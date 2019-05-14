94e50ceb0175904a2b4f0bb94d7098ab44b2aa87
Daniel Brown <browndj3@gmail.com>
2019-05-14 16:14:33 -0500

    Day 19 part 1 complete

---
+++ advent_of_code/2015/Day_19/Part1.py
@@ -0,0 +1,21 @@
+combos = {}
+molecules = set()
+
+with open("input19.txt") as file:
+    for line in file:
+        separated = line.strip().split()
+        if separated[0] not in combos:
+            combos[separated[0]] = []
+        combos[separated[0]].append(separated[2])
+
+with open("input19_2.txt") as file:
+    base = file.read().strip()
+
+for i, let in enumerate(base):
+    if let in combos:
+        for swap in combos[let]:
+            molecules.update([f"{base[:i]}{swap}{base[i + 1:]}"])
+    elif base[i:i+2] in combos:
+        for swap in combos[base[i:i+2]]:
+            molecules.update([f"{base[:i]}{swap}{base[i+2:]}"])
+print(len(molecules))
