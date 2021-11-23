802d4d7e7c6d916132b238401d3b746d9672cc49
Daniel Brown <browndj3@gmail.com>
2021-11-23 16:05:56 -0600

    Adjusted folder structure for consistency.  Moved input_getter.py to make it usable for all advent years and days

--- advent_of_code/input_getter.py
+++ advent_of_code/input_getter.py
@@ -2,10 +2,10 @@ import os
 import requests
 
 
-def get_input(day: int) -> None:
-    input_url = f"https://adventofcode.com/2019/day/{day}/input"
+def get_input(year: int, day: int) -> None:
+    input_url = f"https://adventofcode.com/{year}/day/{day}/input"
 
-    if not os.path.exists(f"inputs/day_{day}_input.txt"):
+    if not os.path.exists(f"year/day_{day}_input.txt"):
         print("Input file not found, retrieving from Advent site.")
         cookies = {"session": os.environ.get("AOC_SESSION")}
         response = requests.get(input_url, cookies=cookies)
