e6373f67b8706cafe9fab4cc707694403c68c125
Daniel Brown <browndj3@gmail.com>
2021-11-29 15:54:59 -0600

    Added directory creation in `input_getter.py`

--- advent_of_code/input_getter.py
+++ advent_of_code/input_getter.py
@@ -10,14 +10,17 @@ def get_input() -> None:
     year = int(input("Which year?  "))
     day = int(input("Which day?  "))
     input_url = f"https://adventofcode.com/{year}/day/{day}/input"
-    input_path = Path(f"{year}/Day_{day}/Day_{day}_input.txt")
+    input_path = Path(f"{year}/Day_{day}/")
+    file_path = input_path / f"Day_{day}_input.txt"
 
-    if not input_path.exists():
+    if not file_path.exists():
+        if not input_path.exists():
+            input_path.mkdir(parents=True)
         print("Input file not found, retrieving from Advent site.")
         cookies = {"session": os.environ.get("AOC_SESSION")}
         response = requests.get(input_url, cookies=cookies)
         if response.ok:
-            with open(input_path, "w") as file:
+            with open(file_path, "w") as file:
                 file.write(response.text)
         else:
             print("Something went wrong")
